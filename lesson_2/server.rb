94aec1d2cc61d3bfba922dcfd57eb2913bb3ea36
mskalisz <mskalisz3@gmail.com>
2022-05-20 14:26:50 -0400

    Create a simple echo server

--- lesson_2/server.rb
+++ lesson_2/server.rb
@@ -7,6 +7,8 @@ loop do
   request_line = client.gets # Gets the FIRST line of the request (the method, GET or POST)
   puts request_line # Print that line to the console
 
+  client.puts "HTTP/1.1 200 OK" #add a valid status line to your response first, before adding the content of the message body for browsers like Chrome which expects a well-formed response to be sent to it for rendering
+  client.puts "Content-Type: text/plain\r\n\r\n" # add in a response header value
   client.puts request_line # Send back to the client so it appears in the web browser
   client.close
 end
