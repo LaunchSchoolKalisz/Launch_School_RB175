aa7548227016ae2f12fb104bbe56a998fdcb52f4
mskalisz <mskalisz3@gmail.com>
2022-05-21 09:14:38 -0400

    Add relationship with Framework class

--- lesson_3/basic_rack_install/app.rb
+++ lesson_3/basic_rack_install/app.rb
@@ -1,7 +1,8 @@
-require_relative 'advice'
 require_relative 'framework'
+require_relative 'advice'
 
-class App
+
+class App < Framework
   def call(env)
     case env['REQUEST_PATH']
     when '/'
@@ -11,9 +12,9 @@ class App
         erb :index
       end
     when '/advice'
-      piece_of_advice = Advice.new.generate
       status = '200'
       headers = {"Content-Type" => 'text/html'}
+      piece_of_advice = Advice.new.generate
       response(status, headers) do
         erb :advice, message: piece_of_advice
       end
