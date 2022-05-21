be190d34a89c2d0aa1520298beab09bd81b531a7
mskalisz <mskalisz3@gmail.com>
2022-05-20 22:00:25 -0400

    Add comment for clarity

--- lesson_3/basic_rack_install/hello_world.rb
+++ lesson_3/basic_rack_install/hello_world.rb
@@ -5,3 +5,7 @@ class HelloWorld
     ['200', {'Content-Type' => 'text/plain'}, ['Hello World!']]
   end
 end
+
+=begin
+We know it is a Rack application because it has the method call(env), and that method returns a 3 element array containing the exact information needed for a proper Rack application: a status code (string), headers (hash), and a response body (responds to each).
+=end
