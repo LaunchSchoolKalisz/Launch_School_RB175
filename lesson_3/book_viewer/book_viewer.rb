f8e995563a4c75d9bcb72881880dcc77fa3214a7
mskalisz <mskalisz3@gmail.com>
2022-05-21 22:54:45 -0400

    Move contents variable into a before filter

--- lesson_3/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer/book_viewer.rb
@@ -2,19 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   @table_of_contents = File.read("data/toc.txt")
-  @contents = File.readlines("data/toc.txt")
   
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   @number = params[:number].to_i
   chapter_name = @contents[@number - 1]
+  
   @title = "Chapter #{@number}: #{chapter_name}"
   @chapter = File.read("data/chp#{@number}.txt")
 
