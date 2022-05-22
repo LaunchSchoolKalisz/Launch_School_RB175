350bd77c621a90b09e4f76042e6dd0dbb305fc2c
mskalisz <mskalisz3@gmail.com>
2022-05-21 20:15:53 -0400

    Update chapter.erb to include the chapter content

--- lesson_3/book_viewer/book_viewer.rb
+++ lesson_3/book_viewer/book_viewer.rb
@@ -12,7 +12,7 @@ end
 
 get "/chapters/1" do
   @title = "Chapter 1"
-  @contents = File.readlines("data/toc/txt")
+  @contents = File.readlines("data/toc.txt")
   @chapter = File.read("data/chp1.txt")
 
   erb :chapter
